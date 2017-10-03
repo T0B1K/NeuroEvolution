149509594666bfa50ede5ef5796797d7a12c071c
T0B1K <31769288+T0B1K@users.noreply.github.com>
2017-10-03 16:23:55 +0200

    commit

---
+++ Main.java
@@ -0,0 +1,33 @@
+import basicMath.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        //test
+        /*
+        Matrix m = new Matrix(2, 2);
+        Vector v = new Vector(2);
+
+        double[] inpv = new double[2];
+        inpv[0] = 1;
+        inpv[1] = 1;
+
+        double[][] inpmat = new double[2][2];
+        inpmat[0][0] = 2;
+        inpmat[1][0] = 2;
+        inpmat[0][1] = 3;
+        inpmat[1][1] = 3;
+
+
+        m.setValues(inpmat);
+        v.setValues(inpv);
+
+        String s = "";
+        for (int i = 0; i < Matrix.dotProductVM(v, m).length; i++) {
+            s += " " + Matrix.dotProductVM(v, m)[i];
+        }
+        System.out.println(s);
+*/
+    }
+}
